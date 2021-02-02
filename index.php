e89804363b67127b821805e1a93fba1566f5ead9
Dean <249661@cursist.rockopnh.nl>
2021-02-02 13:30:52 +0100

    0.1 homepage - placeholders

--- index.php
+++ index.php
@@ -1,1 +1,36 @@
-ufbgal
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <script src=""></script>
+    <link rel="stylesheet" href="mystyle.css">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<img src=".git/images/logop.png" class="logop" alt="logo" width="150" height="150" >
+<!-- Top navigation menu -->
+<div class="menu">
+<a class="active" href="index.php">Home</a>
+<a href="service">Service</a>
+<a href="voorlichting">Voorlichting</a>
+<a href="inlog">Mijn APO (inlog)</a>
+</div>
+<!--slideshow place holder -->
+<img src=".git/images/placeholderimage.png" alt="placeholder" width="600" height="300">
+<!-- google maps map -->
+<div class="map-container">
+    <div class="mapouter">
+    <div class="gmap_canvas">
+        <iframe width="300" height="300" id="gmap_canvas"
+ src="https://maps.google.com/maps?q=schagen&t=&z=13&ie=UTF8&iwloc=&output=embed"
+  frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe>
+    <br>
+    </div>
+    </div>
+</div>
+
+    <?php
+    ?>
+</body>
+</html>
