84eb5c4dbf3a8d585ed9ec1de45369843b81104c
Dean <249661@cursist.rockopnh.nl>
2021-02-10 21:07:54 +0100

    Homepage 0.3

--- index.php
+++ index.php
@@ -1,7 +1,8 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <script src=""></script>
+<script>src="myScript.js"</script>
+    <script> </script>
     <link rel="stylesheet" href="mystyle.css">
     <meta charset="UTF-8">
     <meta name="viewport" content="width=, initial-scale=1.0">
@@ -16,8 +17,53 @@
 include('menu.php')
 ?>
 <!--slideshow place holder -->
-<img src=".git/images/placeholderimage.png" alt="placeholder" width="600" height="300">
-<!-- google maps map -->
+
+<body>  
+    <div class="slidercontainer">  
+        <div class="showSlide fade">  
+            <img src=".git/images/placeholderimage.png" width="600" height="300"> />  
+            <div class="content">Lorem ipsum dolor sit amet</div>  
+        </div>  
+        <div class="showSlide fade">  
+            <img src=".git/images/placeholder2.jpg" width="600" height="300"> />  
+            <div class="content">Lorem ipsum dolor sit amet</div>  
+        </div>  
+  
+        <div class="showSlide fade">  
+            <img src=".git/images/placeholder3.jpg"width="600" height="300"> />  
+            <div class="content">Lorem ipsum dolor sit amet</div>  
+        </div>  
+        <div class="showSlide fade">  
+            <img src=".git/images/logop.png"width="600" height="300"> />  
+            <div class="content">Lorem ipsum dolor sit amet</div>  
+        </div>  
+        <!-- Navigation arrows -->  
+        <a class="left" onclick="nextSlide(-1)">❮</a>  
+        <a class="right" onclick="nextSlide(1)">❯</a>  
+    </div> 
+    <script type="text/javascript">  
+        var slide_index = 1;  
+        displaySlides(slide_index);  
+        function nextSlide(n) {  
+            displaySlides(slide_index += n);  
+        }  
+        function currentSlide(n) {  
+            displaySlides(slide_index = n);  
+        }  
+        function displaySlides(n) {  
+            var i;  
+            var slides = document.getElementsByClassName("showSlide");  
+            if (n > slides.length) { slide_index = 1 }  
+            if (n < 1) { slide_index = slides.length }  
+            for (i = 0; i < slides.length; i++) {  
+                slides[i].style.display = "none";  
+            }  
+            slides[slide_index - 1].style.display = "block";  
+        }  
+</script>   
+</body>   
+
+<!--google maps map -->
 <div class="map-container">
     <div class="mapouter">
     <div class="gmap_canvas">
